c26e2adf4ab6b856e3268ec9aa1da4d8c0bcaec8
Jie Zhou <zhoujie@my.bridgeport.edu>
2017-01-16 00:43:47 -0500

    Create connection to database

--- Mini-JDBC-App/src/com/jdbc/demo/DemoApp.java
+++ Mini-JDBC-App/src/com/jdbc/demo/DemoApp.java
@@ -1,9 +1,29 @@
 package com.jdbc.demo;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
 public class DemoApp {
 
+	Connection con;
+	Statement st;
+	
+	DemoApp() {
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			con = DriverManager.getConnection("jdbc:mysql://localhost:3306/inventory", "root", "root");
+			if(con != null) {
+				System.out.println("Connected!");
+			}
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		new DemoApp();
 
 	}
 
